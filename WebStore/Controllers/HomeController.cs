1f8c30990c16fd40e5f62762cfe8f93c10efc156
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-11-26 14:50:11 +0300

    Passing data to a view as a model.

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -4,12 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IConfiguration _Configuration;
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee() { Id = 1, LastName = "Ivanov", FirstName = "Ivan", Patronymic = "Ivanovich", Age = 28 },
+            new Employee() { Id = 2, LastName = "Petrov", FirstName = "Sergey", Patronymic = "Sergeevich", Age = 23 },
+            new Employee() { Id = 3, LastName = "Bodrova", FirstName = "Marina", Patronymic = "Antonovna", Age = 19 }
+        };
 
         public HomeController(IConfiguration Configuration) => _Configuration = Configuration;
 
@@ -23,6 +30,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction()
         {
             return Content("Second controller action");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees );
         }
     }
 }
