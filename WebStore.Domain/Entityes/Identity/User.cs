a63ed14c61d2da1428bca87485a7aa03589b3308
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-12-13 06:58:21 +0300

    The definition of Administrator and User.

---
+++ WebStore.Domain/Entityes/Identity/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entityes.Identity
+{
+    public class User : IdentityUser
+    {
+        public string Description { get; set; }
+        public const string Administrator = "Administrator";
+        public const string DefaultAdminPassword = "AdPass";
+    }
+    public class Role : IdentityRole
+    {
+        public const string Administrator = "Administrators";
+        public const string User = "Users";
+        public string Description { get; set; }
+    }
+}
